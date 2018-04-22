37a705c3ba1cddfb50a522daa9950139224b7c98
NEO <NEOissss@users.noreply.github.com>
2018-04-22 15:55:34 -0400

    edit

--- lib/datasets/wider.py
+++ lib/datasets/wider.py
@@ -63,9 +63,9 @@ class wider(imdb):
     """
     """
     path = []
-    for root, dirs, files in os.walk(self._data_path):
-        for name in files:
-            path.append(name)
+    for dir_name in os.listdir(self._data_path):
+        for _, _, name in os.walk(os.path.join(self._data_path, dir_name)):
+            path.append(os.path.join(dir_name, name))
     return path
 
   def _get_default_path(self):
